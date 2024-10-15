b9d64b759f6a344fa6382673e0933005026ee945
Napat Phobutdee <dental.anamai@gmail.com>
2024-10-15 21:56:43 +0700

    add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -70,7 +70,7 @@ const App = () => {
   }, [webcamRef]);
 
   // Convert file to Base64
-  const convertToBase64 = (file: Blob) => {
+  const convertToBase64 = (file: unknown) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onloadend = () => {
